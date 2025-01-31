376df8e37d4c8c31cf4746cf361237b90dcd4b2c
Azeem Khan <azeemkh528@gmail.com>
2025-01-31 18:39:51 +0530

    my Controller file updated and swagger spec.yaml file added

--- src/main/java/com/beats/testing/MyController.java
+++ src/main/java/com/beats/testing/MyController.java
@@ -1,4 +1,19 @@
 package com.beats.testing;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
+
+    @GetMapping("/getResponse")
+    public String getResponse(){
+        return "this is a testing text from spring boot";
+    }
+
+    @GetMapping("/send")
+    public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
 }
